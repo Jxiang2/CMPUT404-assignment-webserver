078be47488d1aeb093831ccb43f6147dde5b95a1
Jingyu Xiang <jxiang2@ualberta.ca>
2021-09-17 22:29:46 -0600

    server done

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,10 +29,65 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
+
+    def successful_send(self, res, format):
+        self.request.send("HTTP/1.1 200 OK \n".encode())
+        self.request.send(("Content-type: text/%s \n"%format).encode())
+        self.request.sendall(res.encode())
+
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
+
+        '''
+        The data string is:  GET / HTTP/1.1
+        Accept-Encoding: identity
+        Host: 127.0.0.1:8080
+        User-Agent: Python-urllib/3.8
+        Connection: close
+        '''
+
+        data_string = self.data.decode()
+        print("###################")
+        print("The data string is: ", data_string)
+        print("###################")
+
+        assert (data_string != None), ("HTTP/1.1 400 Bad Request \n".encode())
+        http_method, path = (data_string.split()[0], data_string.split()[1])
+        print(http_method, 'and', path)
+
+        real_path = os.getcwd() + "/www"
+        if http_method == "GET":
+            if os.path.realpath(real_path + path).startswith(real_path):
+                if os.path.exists(real_path + "/index.html") and path.endswith("/"):
+                    resource = open(real_path + "/index.html").read()
+                    format = "html"
+                    self.successful_send(resource, format)
+
+                elif os.path.exists("./www" + path) and path.endswith(".html"):
+                    resource = open("./www" + path).read()
+                    self.successful_send(resource, format="html")
+
+                elif os.path.exists("./www" + path) and path.endswith(".css"):
+                    resource = open("./www" + path).read()
+                    self.successful_send(resource, format="css")
+
+                else:
+                    try:
+                        resource = open("./www" + path + "/index.html")
+
+                        self.request.send("HTTP/1.1 301 Moved Permanently \n".encode())
+                        self.request.sendall(("HTTP://127.0.0.1:8080" + path).encode())
+                    except:
+                        self.request.send("HTTP/1.1 404 Not Found \n".encode())
+                
+            else:
+                self.request.send("HTTP/1.1 404 Not Found \n".encode())
+
+        else:
+            self.request.send("HTTP/1.1 405 Method Not Allowed \n".encode())
+
         self.request.sendall(bytearray("OK",'utf-8'))
 
 if __name__ == "__main__":
